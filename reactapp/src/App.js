00274fb824701f105cdcf0cc559765589500362c
cbedroid <cbedroid1614@gmail.com>
2022-02-20 18:19:45 -0500

    Moved redux initial state method to App.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
-import Header from "./components/Header.jsx";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import { useDispatch } from "react-redux";
+import { fetchAllArtists, fetchAlbums, fetchAllVideos } from "./features/album"
 import Base from "./pages/Base.js"
 import BaseRouter from "./routes.js";
+import Header from "./components/Header.jsx";
 import "./styles/app.css"
 
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    console.log("App loaded")
+    dispatch(fetchAllArtists())
+    dispatch(fetchAlbums())
+    dispatch(fetchAllVideos())
+  })
 
   return (
     <div className="App">
